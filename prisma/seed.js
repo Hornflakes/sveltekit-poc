c96cab1895876dc12645c8b33908f859dab92807
Hornflakes <cristianpetrugug@gmail.com>
2023-12-09 18:25:08 +0200

    chore: fix seed ?

---
+++ prisma/seed.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const roles = [
+        {
+            id: 1,
+            name: 'ADMIN',
+        },
+        {
+            id: 2,
+            name: 'USER',
+        },
+    ];
+
+    for (const role of roles) {
+        await prisma.roles.create({
+            data: role,
+        });
+    }
+}
+
+main()
+    .catch((err) => {
+        console.log(err);
+        throw err;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
